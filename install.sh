670c7b0d0767a5b6b9e3ef885c616aac8d2ed741
miyucy <fistfvck@gmail.com>
2024-08-16 11:35:05 +0900

    Add delta

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 ln -s $(pwd)/dot.bash_aliases ~/.bash_aliases
+
+mkdir -p "$HOME/.local/bin"
+
+curl -LO https://github.com/dandavison/delta/releases/download/0.17.0/delta-0.17.0-aarch64-unknown-linux-gnu.tar.gz
+tar xf delta-0.17.0-aarch64-unknown-linux-gnu.tar.gz
+mv delta-0.17.0-aarch64-unknown-linux-gnu/delta "$HOME/.local/bin"
+rm -rf delta-0.17.0-aarch64-unknown-linux-gnu.tar.gz delta-0.17.0-aarch64-unknown-linux-gnu
