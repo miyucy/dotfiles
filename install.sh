f7eab2c2015e62794be2dd2322fcb247cc175b8d
miyucy <fistfvck@gmail.com>
2024-08-21 11:08:24 +0900

    Update delta

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ ln -s $(pwd)/dot.bash_aliases ~/.bash_aliases
 
 mkdir -p "$HOME/.local/bin"
 
-curl -LO https://github.com/dandavison/delta/releases/download/0.17.0/delta-0.17.0-aarch64-unknown-linux-gnu.tar.gz
-tar xf delta-0.17.0-aarch64-unknown-linux-gnu.tar.gz
-mv delta-0.17.0-aarch64-unknown-linux-gnu/delta "$HOME/.local/bin"
-rm -rf delta-0.17.0-aarch64-unknown-linux-gnu.tar.gz delta-0.17.0-aarch64-unknown-linux-gnu
+t=$(mktemp -d)
+pushd "$t"
+curl -Lq https://github.com/dandavison/delta/releases/download/0.18.0/delta-0.18.0-aarch64-unknown-linux-gnu.tar.gz | tar xzf - --strip-components=1
+mv delta "$HOME/.local/bin"
+popd
+rm -rf "$t"
