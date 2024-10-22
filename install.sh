0e52f0e92215a74a3ff2e40ec672fa369b7d9898
miyucy <fistfvck@gmail.com>
2024-10-22 22:06:08 +0900

    Update delta

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir -p "$HOME/.local/bin"
 
 t=$(mktemp -d)
 pushd "$t"
-curl -Lq https://github.com/dandavison/delta/releases/download/0.18.0/delta-0.18.0-aarch64-unknown-linux-gnu.tar.gz | tar xzf - --strip-components=1
+curl -Lq https://github.com/dandavison/delta/releases/download/0.18.2/delta-0.18.2-aarch64-unknown-linux-gnu.tar.gz | tar xzf - --strip-components=1
 mv delta ~/.local/bin
 popd
 rm -rf "$t"
